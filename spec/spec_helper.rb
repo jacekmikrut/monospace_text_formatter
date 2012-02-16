3c4e33491ec688deadc54205d37be33805613886
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-02-16 09:26:19 +0100

    Now spec_helper requires "support/**/*.rb" files.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,7 @@ require "bundler/setup"
 
 require "monospace_text_formatter"
 
+Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |file| require file }
+
 RSpec.configure do |config|
 end
